d434f2cdc6e5f9cd8b40c565a2a04ee6492ec0da
Mike Cappeller <mikecappeller@gmail.com>
2021-05-20 17:35:29 -0400

    simplify

--- client.js
+++ client.js
@@ -9,22 +9,14 @@ const command_line = readline.createInterface({
 
 
 const connection = net.createConnection({
-  host: '40.114.109.223',
+  host: 'CHANGE TO YOUR AZURE IP',
   port: 5000
 });
 
 const xx = () => {
-  // console.log('Got in touch with the server!');
-
-  // command_line.question("Enter Message:", (message) => {
-  //   connection.write(Buffer.from(message, 'utf8'));
-  // })
   command_line.question("Enter a username: ", (username) => {
-    console.log("Start sending messgaes!");
-    command_line.setPrompt("");
     command_line.prompt();
     command_line.on('line', (message) => {
-      // console.log(`${username}: ${message}`);
       connection.write(Buffer.from(`${username}: ${message}`, 'utf8'));
       command_line.prompt();
     })
@@ -36,7 +28,6 @@ connection.on('connect', () => {
 })
 
 connection.on('data', (data) => {
-  // console.log("Received Data: ", data.toString('utf8'));
   console.log(data.toString('utf8'));
 });
 
--- server.js
+++ server.js
@@ -7,29 +7,19 @@ server.listen({
 })
 
 server.on('connection', (connection) => {
-  console.log("Someone connected")
-  //connection.write(Buffer.from("Hello", 'utf8'))
-  sockets.push(connection);
-  connection.on('data', (data) => {
-    //console.log("Received Data: ", data.toString('utf8'));
-    sockets.forEach(socket => {
-      if (socket !== connection) {
-        socket.write(Buffer.from(data, 'utf8'))
-      }
-    })
-  })
+      console.log("Someone connected")
+      sockets.push(connection);
+      connection.on('data', (data) => {
+        sockets.forEach(socket => {
+          if (socket !== connection) {
+            socket.write(Buffer.from(data, 'utf8'))
+          }
+        })
+      })
 
-  connection.on('end', () => {
-    console.log("Someone left the chat!");
-  })
+      connection.on('end', () => {
+        console.log("Someone left the chat!");
+      })
 
 
-  connection.on('error', function (error) {
-    console.log("Someone left the chat!");
-    //console.log(connection);
-    // console.log('Socket got problems: ', error.message);
-
-  });
-})
-
-console.log("Listening for incoming connections")
+      console.log("Listening for incoming connections")
